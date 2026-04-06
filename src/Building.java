e2ce6454dcddaa07445721c292a5962d86a9532d
naufalimp <naufalrawe3gmailcom@webmail.umm.ac.id>
2026-04-07 00:02:34 +0700

    tugas modul 2

---
+++ src/Building.java
@@ -0,0 +1,36 @@
+
+public class Building {
+    String building;
+    String Addres;
+    int Floor;
+
+    public Building(String building, String Addres, int Floor){
+        this.building = building
+        this.Addres = Addres;
+        this.Floor = Floor;
+
+        displayBuildInfo();
+
+        System.out.println("==============================");
+        System.out.println("Building Added Succesfully");
+    }
+
+    private void displayBuildInfo() {
+        System.out.println("==================================");
+        System.out.println("------ Building Information ------");
+        System.out.println("Name of the building : " + building);
+        System.out.println("Addres of the building : " + Addres);
+        System.out.println("Number Floor of the building : " + Floor);
+        System.out.println("===================================");
+    }
+
+    String getbuilding(){
+        return this.building;
+    }
+    String getAddres(){
+        return this.Addres;
+    }
+    int getNumberFloor(){
+        return this.Floor;
+    }
+}
