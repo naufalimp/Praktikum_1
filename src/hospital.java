559f122195a9a764be9744255b8e869964e86f61
naufalimp <naufalrawe3gmailcom@webmail.umm.ac.id>
2026-04-07 09:48:49 +0700

    tugas modul 3

--- src/hospital.java
+++ src/hospital.java
@@ -1,7 +1,7 @@
-public class Hospital extends Building {
+public class hospital extends Building {
     int NumberOfBeds;
 
-    public Hospital(String Name, String Addres, int Floor, int NumberOfBeds){
+    public hospital(String Name, String Addres, int Floor, int NumberOfBeds){
         super(Name, Addres, Floor);
         this.NumberOfBeds = NumberOfBeds;
 
